87d659467b227cd06fa66aba71f6f5d96a028b38
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-12-25 19:25:45 +0530

    changes done

--- src/main/java/DifferenceRange.java
+++ src/main/java/DifferenceRange.java
@@ -1,18 +1,19 @@
 public class DifferenceRange {
-    public  int differenceOfSum(int m,int n){
-        if(m<0 || n<0) return -1;
+    public int differenceOfSum(int m, int n) {
+        if (m < 0 || n < 0) return -1;
         int divisibleSum = 0;
         int nonDivisibleSum = 0;
-        for(int i = 1; i<=m;i++){
-            if(i % n == 0)
+        for (int i = 1; i <= m; i++) {
+            if (i % n == 0)
                 divisibleSum += i;
             else
                 nonDivisibleSum += i;
         }
         return nonDivisibleSum - divisibleSum;
     }
+
     public static void main(String[] args) {
         DifferenceRange differenceRange = new DifferenceRange();
-        System.out.println(differenceRange.differenceOfSum(20,4));
+        System.out.println(differenceRange.differenceOfSum(18, 2));
     }
 }
