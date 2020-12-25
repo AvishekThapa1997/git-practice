ef400e78c7fe9394e15878a8ca8d3ffea430c815
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-12-25 19:34:07 +0530

    kotlin file added

--- src/main/java/DifferenceRange.java
+++ src/main/java/DifferenceRange.java
@@ -14,6 +14,6 @@ public class DifferenceRange {
 
     public static void main(String[] args) {
         DifferenceRange differenceRange = new DifferenceRange();
-        System.out.println(differenceRange.differenceOfSum(18, 2));
+        System.out.println(differenceRange.differenceOfSum(10, 2));
     }
 }
